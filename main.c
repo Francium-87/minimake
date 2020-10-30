14961e13ecf9027a90cd78a369080b881a5bb386
François HOUSSIN <fransoishoussin@gmail.com>
2020-10-30 16:14:26 +0100

    added file opening with fopen

--- main.c
+++ main.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
 
-struct rule {
+struct rule 
+{
     char* target;
     char* dependencies;
     char* receipe;
 };
 
-struct var {
+struct var 
+{
     char* label;
     char* value;
 };
 
-struct node {
+struct node 
+{
     struct rule r;
     struct var v;
     struct node* next;
 };
 
-int main (int argc, char** argv){
+int main (int argc, char** argv)
+{
+    FILE* f = fopen(argv[1], 'r');
+
+    if(f == NULL)
+    {
+        fprintf(stderr, "[Minimake] Main: Failed to open file %s\n", argv[1]);
+        return(EXIT_FAILURE);
+    }
+
+
     return 0;
 }

45d3b0e2da552e6a0f1c1dfe3d09bbe9d300eb4d
François HOUSSIN <fransoishoussin@gmail.com>
2020-10-30 16:27:02 +0100

    added void str_split v0.1

--- main.c
+++ main.c
@@ -23,6 +23,31 @@ struct node
     struct node* next;
 };
 
+void str_split (char* line, char delimiter, char* part1, char* part2)
+{
+    // get the position of the delimiter
+    int i = 0;
+    while (line[i] != delimiter)
+        i++;
+    
+    part1 = malloc(sizeof(char) * i);
+    part2 = malloc(sizeof(char) * (strlen(line) - i));
+
+    for(int x = 0; x < strlen(line); x++)
+    {
+        // put everything before the delimiter and store it in part1
+        if (x < i)
+        {
+            part1[x] = line[x];
+        }
+        // put everything after the delimiter and store it in part2
+        else if (x > i)
+        {
+            part2[strlen(line) - x] = line[x];
+        }
+    }
+}
+
 int main (int argc, char** argv)
 {
     FILE* f = fopen(argv[1], 'r');
@@ -32,7 +57,5 @@ int main (int argc, char** argv)
         fprintf(stderr, "[Minimake] Main: Failed to open file %s\n", argv[1]);
         return(EXIT_FAILURE);
     }
-
-
     return 0;
 }
